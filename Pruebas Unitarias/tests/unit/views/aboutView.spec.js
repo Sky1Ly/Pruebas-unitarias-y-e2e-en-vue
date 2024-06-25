d4e44ed7ce5ea8db5ff7926cdf2738d6b57b681e
Eider Agudelo <eider841agudelo@hotmail.com>
2024-06-25 15:53:37 -0400

    Se hace la prueba unitaria de about y home

--- Pruebas Unitarias/tests/unit/views/aboutView.spec.js
+++ Pruebas Unitarias/tests/unit/views/aboutView.spec.js
@@ -1,26 +1,14 @@
-import { mount } from '@vue/test-utils'
-import { createRouter, createWebHistory } from 'vue-router'
+import { shallowMount } from '@vue/test-utils'
 
 import AboutView from '@/views/AboutView.vue'
 
 describe('AboutView', () => {
-    test('Probando la existencia del componente o vista AboutView ', async () => {
-        const router = createRouter({
-            history: createWebHistory(),
-            routes: [{
-                path: '/posts',
-                name: 'AboutViewVue',
-                component: AboutView
-            }],
-        })
-        router.push('/posts')
-        await router.isReady()
+    test('Validación de match con el snapshot', () => {
+        //Selección del componente de prueba
+        const wrapper = shallowMount(AboutView)
 
-        const wrapper = mount(AboutView, {
-            global: {
-                plugins: [router]
-            }
-        })
-        expect(wrapper.findComponent(AboutView).exists()).toBe(true)
+
+        //Asersion
+        expect(wrapper.html()).toMatchSnapshot()
     })
 })
